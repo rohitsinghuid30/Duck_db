b7932c65fb9032578b243c0129d061acfa65bd7b
Rohit Singh <rohituid30@gmail.com>
2024-09-04 16:59:57 +0530

    duck.py updated

---
+++ duck.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import duckdb
+
+
+# query 1
+data = duckdb.read_csv("src/data/mydata.csv")
+print(data)
+
+
+print("")
+print(f"data2 result is below")
+# query 2
+data2 = duckdb.sql('SELECT * from "src/data/mydata2.csv";')
+print(data2)
+
+
+print("")
+print(f"data3 result with where clause is below")
+# query 3
+data3 = duckdb.sql('SELECT * from "src/data/mydata2.csv" WHERE age>30')
+print(data3)
+
+
+
+conn = duckdb.connect("src/db/mydb.ddb")
+
+print("")
+# read sql person
+data_sql = duckdb.sql('SELECT * FROM person')
+print(data_sql.fetchone())
+
+
+
